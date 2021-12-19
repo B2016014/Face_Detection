7e0451798a4906a350e8993752f64c6fe0cad4c6
B2016014 <91531422+B2016014@users.noreply.github.com>
2021-12-19 16:01:03 +0530

    Update detect_code.py

--- detect_code.py
+++ detect_code.py
@@ -1,16 +1,17 @@
-#Face detection
+#Face detection in an image
 import cv2
 #using a pre-trained model
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #Read the input image
 img = cv2.imread('test.jpeg')
 #Convert into grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray_image, 1.2, 5)
 #making a rectangle frame in detecting the face
 for (x,y,w,h) in faces:
-    cv2.rect angle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.rect angle(img,(x,y),(x+w,y+h),(0,0,255),2)
 #showing the detected image
-cv2.imshow('img',img)
-#holding the output window till enter key is pressed
+cv2.imshow('detected_image',img)
+#holding the output window till Enter key is pressed
 cv2.waitKey(0)
+cv2.destroyAllWindows()
