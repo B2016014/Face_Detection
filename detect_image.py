1b1b87db8f39ef5c98b150173d1aebce4b96a043
B2016014 <91531422+B2016014@users.noreply.github.com>
2021-12-21 21:06:45 +0530

    Update detect_image.py

--- detect_image.py
+++ detect_image.py
@@ -2,16 +2,16 @@
 import cv2
 #using a pre-trained model
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#Read the input image
-img = cv2.imread('test.jpeg')
-#Convert into grayscale
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray_image, 1.2, 5)
-#making a rectangle frame in detecting the face
+#Read the input image as a gray scale image
+img = cv2.imread('test.jpeg',0)
+#To detect the faces----returns a list of rectangle coordinates
+faces = face_cascade.detectMultiScale(img, 1.2, 5)
+#making a rectangle  in detected face
 for (x,y,w,h) in faces:
-    cv2.rect angle(img,(x,y),(x+w,y+h),(0,0,255),2)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
 #showing the detected image
 cv2.imshow('detected_image',img)
 #holding the output window till Enter key is pressed
 cv2.waitKey(0)
+#closing all the windows
 cv2.destroyAllWindows()
